83d9746c12b724c908bed0efaac63cba887658db
karinabolat <karina.bolatt@gmail.com>
2021-11-10 23:25:34 -0600

    Updated code

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Project-6-portfolio-site
  Techdegree project 6
+
+The objective is to build a portfolio website to showcase the existing projects. The project in contrast to previous tasks focuses on server-side JavaScript. Technologies used: Node.js, Express, Pug, Git, Github pages.
+
+Customized CSS features: to lessen the sharpness of the contrast between black and white, black color and background was replaced by Medium Jungle Green; image borders were also changed to Nyanza to echo darkgeen palette. Finally, on mouse hover images get a darkgreen border to aid visual focus.
---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+app.set('view engine', 'pug');
+app.use('/static', express.static('public'));
+const {projects} = require('./data.json');
+
+app.get('/', (req, res) => {
+    res.render('index', {projects});
+})
+
+app.get('/about', (req, res) => {
+    res.render('about');
+})
+
+app.get('/project/:id', (req, res, next) => {
+    const idProject = req.params.id;
+    const project = projects.find( ({ id }) => id === +idProject );
+    
+    if (idProject < projects.length) {
+        const technologies = project.technologies;
+        res.render('project', {project, technologies});
+    } else {
+        const err = new Error();
+        err.message = "Ooops, we could not find such project";
+        err.status = 404;
+        next(err);
+    }
+})
+
+
+// Error handlers
+
+app.use((req, res, next) => {
+    console.log("404 error handler");
+    const err = new Error();
+    err.message = 'Oops, page not found';
+    err.status = 404;
+    console.log(err.status, err.message);
+    next(err);
+});
+  
+
+app.use((err, req, res, next) => {
+   if (err) {
+       console.log("Global error handler");
+   }
+   
+   if (err.status === 404) {
+       res.status(err.status)
+       console.log(err.status, err.message);
+       res.render('page-not-found', {err});
+   } else {
+       err.message = err.message || "there is a problem with a server";
+       err.status = err.status || 500;
+       res.status(err.status);
+       console.log(err.status, err.message);
+       res.render('error', {err});      
+   }
+});
+
+app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+})
